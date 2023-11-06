66a504bf2723f958e236bcf463e7ae577e2ed99d
sbsehdgus <sbsehdgus@gmail.com>
2023-11-06 12:19:19 +0900

    test

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll("section");
+
+function scrollHandler(){
+    for(let i = 0; i < sections.length; i++){
+        if(sections[i].offsetTop - 400 <= window.pageYOffset){
+            sections[i].classList.add("active")
+        }
+    }
+}
+
+
+window.addEventListener("load", scrollHandler)
+window.addEventListener("scroll", scrollHandler)
